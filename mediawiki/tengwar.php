360f83b619275f42ac09e0f9c201b262eb949c79
Kris Kowal <kris.kowal@cixar.com>
2012-08-31 13:28:20 -0700

    JavaScript required for MediaWiki extension

--- mediawiki/tengwar.php
+++ mediawiki/tengwar.php
@@ -9,7 +9,7 @@ function wfTengwarInit(Parser $parser) {
 function wfTengwar($input, array $args) {
     $mode = array_key_exists("mode", $args) ? $args["mode"] : "";
     $encoded = array_key_exists("encoded", $args) ? $args["encoded"] : "";
-    $bindings = array_key_exists("bindings", $args) ? $args["bindings"] : "";
+    $bindings = array_key_exists("bindings", $args) ? $args["bindings"] : "(JavaScript required)";
     return "<span class=\"tengwar\" data-tengwar=\"" .
         htmlspecialchars($input) .
         "\" data-encoded=\"" .
