ac5deee52ee73ef961b32b30f26abdb9316e7e51
Kris Kowal <kris@cixar.com>
2019-10-12 14:46:29 -0700

    Change alternative notation from apostrophe to backtick

--- editor/index.webpack.js
+++ editor/index.webpack.js
@@ -1,10 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './index.js',
-    mode: 'production',
+    entry: "./index.js",
+    mode: "production",
     output: {
-        path: path.resolve(__dirname, 'webpack'),
-        filename: 'index.js',
+        path: path.resolve(__dirname, "webpack"),
+        filename: "index.js",
     }
 };
