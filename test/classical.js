b939a6eea110d1392a1b0490c2119ae4b68fa27b
Kris Kowal <kris@cixar.com>
2020-12-25 12:01:03 -0800

    Fix rendering of nwalme in the classical mode (#65)

--- test/classical.js
+++ test/classical.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "eldarinwa": "short-carrier:e;alda:a;romen:i;numen;wilya:a", // suggested by arnog
     "hlóce": "halla;lambe:ó;calma:e", // attested
     "hríve": "halla;romen;long-carrier:i;vala:e", // attested
     "hyarmen": "hyarmen:a,y-quenya;ore;malta:e;numen",
@@ -6,6 +7,7 @@ module.exports = {
     "moria": "malta:o;romen:i;short-carrier:a",
     "namaarië": "numen:a;malta;long-carrier:a;romen:i;short-carrier:e",
     "namárië": "numen:a;malta;long-carrier:a;romen:i;short-carrier:e",
+    "nwalme": "nwalme:a;lambe;malta:e",
     "quenya": "quesse:e;numen:a,y-quenya",
     "silme": "silme-nuquerna:i;lambe;malta:e",
     "syi": "silme:y-quenya;short-carrier:i", //*
@@ -13,6 +15,7 @@ module.exports = {
     "tyelpe": "tinco:e,y-quenya;lambe;parma:e",
     "unque": "short-carrier:u;unque:e",
     "vanwa": "vala:a;numen;wilya:a",
+    "yonwa": "anna:o,y-quenya;numen;wilya:a", // suggested by arnog
     "yuldar": "anna:u,y-quenya;alda:a;ore",
 
     // long vowels
